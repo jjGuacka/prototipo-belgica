44dcae2224830d8f656e06bd200761d9e43de3e8
jjGuacka <57268596+JJGuaca@users.noreply.github.com>
2024-08-15 10:23:11 -0500

    Update settings.php

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -876,8 +876,27 @@ $settings['migrate_node_migrate_type_classic'] = FALSE;
 # $settings['migrate_file_private_path'] = '';
 
 // Automatically generated include for settings managed by ddev.
-if (getenv('IS_DDEV_PROJECT') == 'true' && file_exists(__DIR__ . '/settings.ddev.php')) {
+// if (getenv('IS_DDEV_PROJECT') == 'true' && file_exists(__DIR__ . '/settings.ddev.php')) {
+//   include __DIR__ . '/settings.ddev.php';
+// }
+if (file_exists(__DIR__ . '/settings.ddev.php') && getenv('IS_DDEV_PROJECT') == 'true') {
   include __DIR__ . '/settings.ddev.php';
+}
+
+if (file_exists($app_root . '/' . $site_path . '/settings.dev.php')) {
+  include $app_root . '/' . $site_path . '/settings.dev.php';
+}
+
+if (file_exists($app_root . '/' . $site_path . '/settings.qa.php')) {
+  include $app_root . '/' . $site_path . '/settings.qa.php';
+}
+
+if (file_exists($app_root . '/' . $site_path . '/settings.prod.php')) {
+  include $app_root . '/' . $site_path . '/settings.prod.php';
+}
+
+if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
+  include $app_root . '/' . $site_path . '/settings.local.php';
 }
 
 /**
