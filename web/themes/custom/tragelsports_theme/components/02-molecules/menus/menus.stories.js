b604e4be62bc963f38df7a2c5c637fe019783b3a
jjGuacka <57268596+JJGuaca@users.noreply.github.com>
2024-08-16 12:50:49 -0500

    Ajustes de automatización de estilos y procesos Js

--- web/themes/custom/tragelsports_theme/components/02-molecules/menus/menus.stories.js
+++ web/themes/custom/tragelsports_theme/components/02-molecules/menus/menus.stories.js
@@ -22,3 +22,4 @@ export const inline = () => inlineMenu(inlineMenuData);
 export const main = () => mainMenu(mainMenuData);
 
 export const social = () => socialMenu(socialMenuData);
+
---
+++ web/themes/custom/tragelsports_theme/js/script.js
@@ -0,0 +1,2 @@
+let prueba=document.querySelectorAll(".prueba");for(var i=0;i<prueba.length;i++)prueba[i].classList.add("prueba"+i),console.log(prueba[i]);
+//# sourceMappingURL=script.js.map
