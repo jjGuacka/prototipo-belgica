72ba2e22a19a55029c0ff403c6738d727d3debf2
jjGuacka <57268596+JJGuaca@users.noreply.github.com>
2024-09-17 20:56:10 -0500

    Menú priccipal

--- web/themes/custom/tragelsports_theme/components/script.js
+++ web/themes/custom/tragelsports_theme/components/script.js
@@ -1,1 +1,28 @@
+document.addEventListener('DOMContentLoaded', function(){
+    let location  = window.location.href;
+    activeMenu(location);
+})
 
+function activeMenu(location){
+    let cadena = location.split('/',4);
+    cadena = cadena[3];
+    let menuPpal = document.getElementById('navbarNavDropdown');
+    let itemsMenduMain = menuPpal.querySelectorAll('.itemMenuMain');
+    for(let i =0; i< itemsMenduMain.length; i++){
+        if(itemsMenduMain[i].children[0].pathname === '/'+cadena){
+            addActive(itemsMenduMain[i].children[0], cadena)
+        }
+    }
+}
+function addActive(valor,cadena){
+    if(valor.innerText.toLowerCase() === cadena){
+        valor.parentNode.classList.add('active');
+    }else if(valor.innerText.toLowerCase() == "home"){
+        let menuPpal = document.getElementById('navbarNavDropdown');
+        let itemsMenduMain = menuPpal.querySelectorAll('.itemMenuMain');
+        itemsMenduMain[0].classList.add('active');
+    }else if(valor.innerText.toLowerCase() == "stengrill"||valor.innerText.toLowerCase() == "kidsparty"||valor.innerText.toLowerCase() == "feesten"){
+        console.log(cadena)
+    }
+    
+}
