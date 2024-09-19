a428848e6c2ceca443ed8b1c64139cd06a7e0bda
jjGuacka <57268596+JJGuaca@users.noreply.github.com>
2024-09-18 19:23:23 -0500

    Sliders y tarjetas del home

--- web/themes/custom/tragelsports_theme/components/script.js
+++ web/themes/custom/tragelsports_theme/components/script.js
@@ -5,14 +5,18 @@ document.addEventListener('DOMContentLoaded', function(){
 
 function activeMenu(location){
     let cadena = location.split('/',6);
-    console.log(cadena.reverse());
     cadena = cadena[0];
     let menuPpal = document.getElementById('navbarNavDropdown');
     let itemsMenduMain = menuPpal.querySelectorAll('.itemMenuMain');
     for(let i =0; i< itemsMenduMain.length; i++){
         if(itemsMenduMain[i].children[0].pathname === '/prototipo/web/'+cadena){
             addActive(itemsMenduMain[i].children[0], cadena)
+            
         }
+    }
+    let titulo = document.getElementById('block-tragelsports-theme-page-title');
+    if(titulo.children[0].innerText.toLowerCase() == "home"){
+        titulo.classList.add('d-none');
     }
 }
 function addActive(valor,cadena){
@@ -22,8 +26,22 @@ function addActive(valor,cadena){
         let menuPpal = document.getElementById('navbarNavDropdown');
         let itemsMenduMain = menuPpal.querySelectorAll('.itemMenuMain');
         itemsMenduMain[0].classList.add('active');
-    }else if(valor.innerText.toLowerCase() == "stengrill"||valor.innerText.toLowerCase() == "kidsparty"||valor.innerText.toLowerCase() == "feesten"){
-        console.log(cadena)
-    }
+    }else if(valor.innerText.toLowerCase() == "stengrill"||valor.innerText.toLowerCase() == "kidsparty"||valor.innerText.toLowerCase() == "feesten"){}
     
 }
+
+let items = document.querySelectorAll('.carousel .carousel-item')
+
+items.forEach((el) => {
+    const minPerSlide = 4
+    let next = el.nextElementSibling
+    for (var i=1; i<minPerSlide; i++) {
+        if (!next) {
+            // wrap carousel by using first child
+        	next = items[0]
+      	}
+        let cloneChild = next.cloneNode(true)
+        el.appendChild(cloneChild.children[0])
+        next = next.nextElementSibling
+    }
+})
